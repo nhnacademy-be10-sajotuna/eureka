dc03dc1bf49d56f1dc00e9f300adac99d604dbbe
jeonseung-u <junseungu74@naver.com>
2025-06-09 16:21:40 +0900

    commit

---
+++ src/test/java/com/sajotuna/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajotuna.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
